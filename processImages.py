65b47acb2b03e9ec5aa066f3772f95d236ea1d56
Carlos Vieira <dv60682@gmail.com>
2025-10-14 21:23:49 -0400

    training images processed

--- processImages.py
+++ processImages.py
@@ -1,6 +1,5 @@
 import os
 from PIL import Image
-import uuid
 
 class ImageProcessor:
     def __init__(self):
@@ -14,10 +13,7 @@ class ImageProcessor:
             image = Image.open(image_path)
             resized_image = image.resize(self.target_size)
             pgm_image = resized_image.convert("L")
-            unique_id = str(uuid.uuid4())
-            filename = f"{unique_id}.jpg"
-            final_output_path = os.path.join(os.path.dirname(output_path), filename)
-            pgm_image.save(final_output_path)
+            pgm_image.save(output_path)
         except Exception as e:
             print(f"Error procesando imagen {image_path}: {str(e)}")
         
@@ -30,7 +26,7 @@ class ImageProcessor:
             # Crear directorio de salida si no existe
             os.makedirs(output_subdir, exist_ok=True)
             for filename in os.listdir(input_subdir):
-                if filename.endswith(".png") or filename.endswith(".jpg"):
+                if any(filename.lower().endswith(ext) for ext in ['.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff', '.tif', '.webp']):
                     input_path = os.path.join(input_subdir, filename)
                     output_path = os.path.join(output_subdir, filename)
                     self.resize_image(input_path, output_path)
